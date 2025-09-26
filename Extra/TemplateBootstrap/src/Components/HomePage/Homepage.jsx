1ba74b5b7cdf236c769a86a71ba49917b5cfc3ff
vidhiagrawa <vidhiagrawal2205@gmail.com>
2025-09-26 18:21:35 +0530

    adding

--- Extra/TemplateBootstrap/src/Components/HomePage/Homepage.jsx
+++ Extra/TemplateBootstrap/src/Components/HomePage/Homepage.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
-import homeImg from "/assets/homeimg.jpg"
-import { Button } from 'react-bootstrap'; 
+import React from 'react';
+import homeImg from "/assets/homeimg.jpg";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "./Homepage.css";
+import { Button } from 'react-bootstrap'; 
 function Homepage() {
   return (
     <>
-        <div class="bg-img text-white " style={{position: "relative" ,backgroundImage: `url("/assets/homeimg.jpg")`, minHeight: "50vh", backgroundSize: "cover", 
-    }}>
-        </div>
-        <div className="color-overlay d-flex justify-content-center align-item-center">
+      <div className='container-fluid p-0 position-relative d-flex justify-content-center align-items-center'>
+        <img src={homeImg} className="img-responsive image-con w-100" alt="background" />
+
+        {/* Centered content */}
+        <div className="overlay-text text-center">
           <h1>AI Execs</h1>
+          <p>Mergers and Acquisitions in Artificial <br />Intelligence and Associated Technologies</p>
+          <Button 
+            style={{
+              backgroundColor: '#FF4512',
+              color: '#fff',
+              fontWeight: 'bold',
+              padding: '12px 24px',   // responsive sizing instead of fixed height/width
+              fontSize: '1rem',       // scales with screen size
+              borderRadius: '6px',    // optional styling
+              width: '100%',          // makes it adapt inside parent container
+              maxWidth: '250px',      // prevents it from getting too big
+            }}
+          >
+            Request Report
+          </Button>
         </div>
-            {/* <img src={homeImg} alt="..."/> */}
+      </div>
     </>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
--- Extra/TemplateBootstrap/src/Components/Navbar/Navbar.jsx
+++ Extra/TemplateBootstrap/src/Components/Navbar/Navbar.jsx
@@ -1,31 +1,96 @@
-import { useState } from 'react'
-import "./Navbar.css"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from "/assets/logo.png"
-import { Button } from 'react-bootstrap'; 
-function Navbar(){
-    return(
-        <nav class="navbar navbar-expand-lg bg-white p-3">
-            <div class="container-fluid">
-                <img src={logo}  width="135" height="75"/>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse h-100" id="navbarNav">
-                    <div className='d-flex ps-4 justify-content-around align-item-center w-100 '>
-                        <div className='flex-fill fw-semibold text-center'>Seller</div>
-                        <div className='flex-fill fw-semibold text-center'>Buyers</div>
-                        <div className='flex-fill fw-semibold text-center'> Valuations</div>
-                        <div className='flex-fill fw-semibold text-center'>Deal Flow</div>
-                        <div className='flex-fill fw-semibold text-center'>Markets</div>
-                        <div className='flex-fill fw-semibold text-center'>Investment</div>
-                        <div className='flex-fill fw-semibold text-center'>Geography</div>
-                        <div className='flex-fill fw-semibold text-center'>Advisors</div>
-                    </div>
-                </div>
-            </div>
-            
-        </nav>
-    )
+import { useState } from "react";
+import "./Navbar.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "/assets/logo.png";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar bg-white px-3">
+        <div className="container-fluid">
+          <img src={logo} width="90" height="50" alt="Logo" />
+
+          {/* Toggle button for sidebar */}
+          <button
+            className="btn navbar-toggler btn-outline-light"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </nav>
+
+      {/* Sidebar / Offcanvas */}
+      <div
+        className="offcanvas offcanvas-start text-bg-dark"
+        tabIndex="-1"
+        id="offcanvasNavbar"
+        aria-labelledby="offcanvasNavbarLabel"
+      >
+        <div className="offcanvas-header">
+          <h5 className="offcanvas-title text-white" id="offcanvasNavbarLabel">
+            Menu
+          </h5>
+          <button
+            type="button"
+            className="btn-close btn-close-white"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+
+        <div className="offcanvas-body p-0">
+          <ul className="nav flex-column">
+            <li className="nav-item">
+              <a className="nav-link text-white px-3" href="#">
+                Sellers
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white px-3" href="#">
+                Buyers
+              </a>
+            </li>
+            <li className="nav-item bg-danger">
+              {/* Active / Highlighted item */}
+              <a className="nav-link text-white px-3" href="#">
+                Valuations
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white px-3" href="#">
+                Deal Flow
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white px-3" href="#">
+                Markets
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white px-3" href="#">
+                Investments
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white px-3" href="#">
+                Geography
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white px-3" href="#">
+                Advisors
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
-export default Navbar
+
+export default Navbar;
